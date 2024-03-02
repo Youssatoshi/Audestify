287fda374b82b5db198e03ee8ad443e21d7013a7
bouboulahcen@gmail.com <bouboulahcen@gmail.com>
2024-03-02 13:57:05 +0100

    Added host Heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,7 +29,7 @@ Rails.application.configure do
 
   # Do not fall back to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
-  config.action_mailer.default_url_options = { host: 'audestify.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'https://audestify-2732bee543d3.herokuapp.com/' }
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = "http://assets.example.com"
 
