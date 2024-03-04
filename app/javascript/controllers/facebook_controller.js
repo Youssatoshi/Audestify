8967757332f3d5932853565ff1c31af0f5981877
bouboulahcen@gmail.com <bouboulahcen@gmail.com>
2024-03-04 23:38:52 +0100

    Added facebook login function

---
+++ app/javascript/controllers/facebook_controller.js
@@ -0,0 +1,34 @@
+import { Controller } from "stimulus";
+
+export default class extends Controller {
+  static targets = ["status"]
+
+  connect() {
+    this.checkLoginState();
+  }
+
+  checkLoginState() {
+    FB.getLoginStatus(response => {
+      this.statusChangeCallback(response);
+    });
+  }
+
+  statusChangeCallback(response) {
+    if (response.status === 'connected') {
+      this.statusTarget.textContent = 'Facebook Connected';
+      console.log("User is logged in and authenticated", response);
+    } else if (response.status === 'not_authorized') {
+      this.statusTarget.textContent = 'Please log into this app.';
+      console.log("The user is logged in to Facebook, but not your app", response);
+    } else {
+      this.statusTarget.textContent = 'Please log into Facebook.';
+      console.log("The user isn't logged in to Facebook.", response);
+    }
+  }
+
+  login() {
+    FB.login(response => {
+      this.checkLoginState();
+    }, {scope: 'public_profile,email'});
+  }
+}
