be089dc5af958034f43569d024fee97855c3c810
bouboulahcen@gmail.com <bouboulahcen@gmail.com>
2024-03-05 22:29:04 +0100

    Fixed the JS Controller for Facebook Login

--- app/javascript/controllers/facebook_controller.js
+++ app/javascript/controllers/facebook_controller.js
@@ -29,6 +29,7 @@ export default class extends Controller {
   login() {
     FB.login(response => {
       this.checkLoginState();
-    }, {scope: 'public_profile,email'});
+    }, {scope: 'public_profile,email,instagram_basic,instagram_graph_user_media,instagram_manage_insights,pages_show_list'});
   }
+
 }
