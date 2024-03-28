756c32c07d93d1a01c57cc7ef1f7ab6bfe5e6084
bouboulahcen@gmail.com <bouboulahcen@gmail.com>
2024-03-28 15:05:44 +0100

    Last Polishing

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -9,10 +9,3 @@ eagerLoadControllersFrom("controllers", application)
 // Lazy load controllers as they appear in the DOM (remember not to preload controllers in import map!)
 // import { lazyLoadControllersFrom } from "@hotwired/stimulus-loading"
 // lazyLoadControllersFrom("controllers", application)
-const darkMode = document.querySelector('.dark-mode');
-
-//darkMode.addEventListener('click', () => {
-   // document.body.classList.toggle('dark-mode-variables');
-    //darkMode.querySelector('span:nth-child(1)').classList.toggle('active');
-    //darkMode.querySelector('span:nth-child(2)').classList.toggle('active');/
-//});
--- app/javascript/navabar.js
+++ app/javascript/navabar.js
@@ -1,7 +1,14 @@
-const darkMode = document.querySelector('.dark-mode');
+document.addEventListener("turbo:load", function() {
+  const darkMode = document.querySelector('.dark-mode');
 
-darkMode.addEventListener('click', () => {
-    document.body.classList.toggle('dark-mode-variables');
-    darkMode.querySelector('span:nth-child(1)').classList.toggle('active');
-    darkMode.querySelector('span:nth-child(2)').classList.toggle('active');
+  // Ensure we're not adding multiple listeners after each turbo:load event
+  if (darkMode.getAttribute("listener") !== "true") {
+    darkMode.addEventListener('click', () => {
+      document.body.classList.toggle('dark-mode-variables');
+      darkMode.querySelector('span:nth-child(1)').classList.toggle('active');
+      darkMode.querySelector('span:nth-child(2)').classList.toggle('active');
+    });
+
+    darkMode.setAttribute("listener", "true");
+  }
 });
